f0c19912a3d421be1042b3fbc53e2f927a12b625
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-12 13:06:53 +0200

    Create API endpoint /tweeets to return a list of tweets based on query

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000
+const axios = require('axios');
 
-app.get('/', (req,res) => {
-    res.send("hello world");
+app.get('/tweets', (req,res) => {
+    console.log(req.query)
+    const query = req.query.query
+    const max_results = req.query.max_results
+
+    axios.get("https://api.twitter.com/2/tweets/search/recent",  {
+        params: {
+            query: query,
+            max_results: max_results
+        },
+        headers: {
+            "Authorization": ""
+        }
+    }).then((response) =>{
+            res.status(200).send(response.data)
+        }).catch((error)=>{
+            res.status(400).send(error);
+        })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
